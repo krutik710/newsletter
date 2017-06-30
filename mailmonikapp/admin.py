c1a435681b03f87118dc1fa14985e0ede999da55
krutik <menkudlekrutik@gmail.com>
2017-06-30 23:36:10 +0530

    is_active thing read-only

--- mailmonikapp/admin.py
+++ mailmonikapp/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from mailmonikapp.models import Newsletter,Subscription
 # Register your models here.
 
+class SubscriptionAdmin(admin.ModelAdmin):
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return self.readonly_fields + ('is_active',)
+        return self.readonly_fields
+
+
 admin.site.register(Newsletter)
-admin.site.register(Subscription)
+admin.site.register(Subscription,SubscriptionAdmin)
